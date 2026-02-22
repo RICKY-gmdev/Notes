ef54414491dbd87f2441b158e7a39d277cd44fb7
Md Ayman Iqbal <rickymessi980@gmail.com>
2026-02-22 22:56:52 +0530

    Refactor namespace and using directives in AppDbContext

--- Notes.Api/Data/AppDbContext.cs
+++ Notes.Api/Data/AppDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using ftest1.Notes.Api.Models;
+using Notes.Api.Models;
 
-namespace ftest1.Notes.Api.Data
+namespace Notes.Api.Data
 {
     public class AppDbContext : DbContext
     {
